48b95ba9861cbc516e4f2f0c6cdf67150d81b968
Samuel Joshua <samuel.joshua@g.globo>
2023-12-01 08:19:45 -0300

    feat: alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,12 +20,19 @@ def run_game():
     ship = Ship(ai_settings, screen)
     #Cria um grupo no qual serão armazenado os projéteis
     bullets = Group()
+    # Cria um grupo de aliens
+    aliens = Group()
+
+    # Cria a frota de aliens
+    gf.create_fleet(ai_settings, screen,ship, aliens)
+    
     #inicia o laco principal do jogo
     while True:
       gf.check_events(ai_settings, screen, ship, bullets)
       ship.update()
       gf.update_bullets(bullets)
-      gf.update_screen(ai_settings, screen, ship, bullets)
+      gf.update_aliens(ai_settings, aliens)
+      gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
       
       
--- game_functions.py
+++ game_functions.py
@@ -1,7 +1,8 @@
 import sys
-
 import pygame
+
 from bullet import Bullet 
+from alien import Alien 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Responde a pressionamentos de tecla."""
@@ -11,6 +12,8 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
+    elif event.key == pygame.K_q:
+        sys.exit()
         
 def check_keyup_events(event, ship):
     """Responde a solturas de tecla"""
@@ -37,7 +40,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
                
         
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Atualiza as imagens na tela e alterna para a nova tela"""
     #Redesenha a tela a cada passagem do laço
     screen.fill(ai_settings.bg_color) 
@@ -46,8 +49,9 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
         
     ship.blitme()
-    # Deixa a tela mais recente visivel   
+    aliens.draw(screen)
     
+    # Deixa a tela mais recente visivel   
     pygame.display.flip()
 
 def update_bullets(bullets):
@@ -58,3 +62,60 @@ def update_bullets(bullets):
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+            
+def get_number_aliens_x(ai_settings, alien_width):
+    """Determina o número de aliens que cabe em uma linha"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def get_number_rows(ai_settings, ship_height, alien_height):
+    """Determina o número de linhas com alienígenas que cabem na tela."""
+    available_space_y = (ai_settings.screen_height - (3 * alien_height) - ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+    
+    
+def create_alien(ai_settings, screen, aliens, alien_number, row_number):
+    """Cria um alien e posiciona na linha"""  
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+    aliens.add(alien)
+    
+def create_fleet(ai_settings, screen, ship, aliens):
+    """Cria uma frota completa de aliens"""
+    # Cria um alien e calcula o número de aliens em uma linha
+    # O espaçamento entre os aliens é igual a largura de um alien
+    alien = Alien(ai_settings, screen)
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+    
+    # Cria a primeira linha de aliens
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings, screen, aliens, alien_number,row_number)
+
+def check_fleet_edges(ai_settings, aliens): 
+    """Responde apropriadamente se algum alienígena alcançou uma borda."""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+            
+def change_fleet_direction(ai_settings, aliens):
+    """Faz toda a frota descer e mudar a sua direção"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+        ai_settings.fleet_direction *= -1
+        
+        
+def update_aliens(ai_settings, aliens):
+    """Verifica se a frota está em uma borda"""
+    """ e então atualiza as posições de todos os alienígenas da frota. """
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+    
+    
